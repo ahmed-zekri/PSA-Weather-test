a8e705adc6611e34eaded7e74c37064326a11ad4
ahmed <ahmed.zekri.a@gmail.com>
2022-07-18 01:59:15 +0100

    Dao implementation

---
+++ app/src/main/java/com/example/psaweathertest/data/data_source/WeatherDao.kt
@@ -0,0 +1,24 @@
+package com.example.psaweathertest.data.data_source
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.psaweathertest.domain.model.Weather
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface WeatherDao {
+
+    @Query("SELECT * FROM weather")
+    fun getData(): Flow<List<Weather>>
+
+    @Query("SELECT * FROM weather WHERE city=:cityName")
+    suspend fun getDataByCityName(cityName: String): Weather?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertData(weather: Weather)
+
+
+}
--- app/src/main/java/com/example/psaweathertest/domain/model/Weather.kt
+++ app/src/main/java/com/example/psaweathertest/domain/model/Weather.kt
@@ -1,6 +1,20 @@
 package com.example.psaweathertest.domain.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-data class Weather(val main: String, val description: String, val icon: String)
+data class Weather(
+    val city: String,
+    val main: String,
+    val description: String,
+    val icon: String,
+    val temp: Int,
+    val temp_min: Int,
+    val temp_max: Int,
+    val pressure: Int,
+    val humidity: Int,
+    val windSpeed: Int,
+    val windDegree: Int,
+    @PrimaryKey val id: Int? = null
+)
