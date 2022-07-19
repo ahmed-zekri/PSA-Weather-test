7d6ae8de78b2a5b932247e71aa437f175ccfb87d
ahmed <ahmed.zekri.a@gmail.com>
2022-07-19 21:57:41 +0100

    Dokka documentation : adding description for FetchWeather invoke method that will be used in app module to get the weather data

---
+++ documentation/html/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":open_weather_api:dokkaHtml/androidTestRelease":[],":open_weather_api:dokkaHtml/debug":[],":open_weather_api:dokkaHtml/main":[],":open_weather_api:dokkaHtml/release":[],":open_weather_api:dokkaHtml/testFixtures":[],":open_weather_api:dokkaHtml/testFixturesDebug":[],":open_weather_api:dokkaHtml/testFixturesRelease":[]}'
--- open_weather_api/src/main/java/com/example/open_weather_api/domain/use_cases/FetchWeather.kt
+++ open_weather_api/src/main/java/com/example/open_weather_api/domain/use_cases/FetchWeather.kt
@@ -12,6 +12,13 @@ enum class FetchingMethod {
 
 class FetchWeather @Inject constructor(val remoteWeatherRepository: RemoteWeatherRepository) {
 
+    /**
+     *Return the weather details of the given city , the input can be either the city name as a String or the the geographic coordinates as a pair of doubles
+     *
+     *@param data : A string representing the city name must be provided if the fetchingMethod enum value provided is BY_NAME, otherwise , a pair of doubles must be provided, providing the wrong input will lead to an IllegalStateException
+     * @param fetchingMethod : BY_COORDINATES or BY_NAMES should be provided. if not provided , BY_NAMES will be taken by default
+     */
+
     suspend inline operator fun invoke(
         data: Any,
         fetchingMethod: FetchingMethod = FetchingMethod.BY_NAME
