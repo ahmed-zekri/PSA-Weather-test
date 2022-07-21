df3306af4434ccc54c455a54a523c1342fd20134
ahmed <ahmed.zekri.a@gmail.com>
2022-07-21 19:05:19 +0100

    remove unused dependency

--- app/src/main/java/com/example/psaweathertest/presentation/cities_list/components/CitiesListScreen.kt
+++ app/src/main/java/com/example/psaweathertest/presentation/cities_list/components/CitiesListScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
